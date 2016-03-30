3e5ef56513669a37427aaf82660835d7f91207d3
Kimberly Stickley <kstickley01@aurora.edu>
2016-03-30 12:24:22 -0500

    Updating home.php
    Adding a stylesheet

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<link rel="stylesheet" href="synco.css">
+</head>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <body>
 
