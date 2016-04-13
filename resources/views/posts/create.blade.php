499c1fe7a5daa77b850530812b792fb404a9ad8f
jacobtarter <jtarter01@aurora.edu>
2016-04-13 03:42:36 -0500

    new post form updates

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,13 @@
 <body>
 
 <p style="font-size: 24px"><b>Post New Comment!</b></p>
-
+{!! Form::open(array('route' => 'posts.store')) !!}
+    {{ Form::label('title', 'Title: ') }}
+    {{ Form::text('title', null) }}
+    {{ Form::label('posttext', "Post Text: ") }}
+    {{ Form::textarea('posttext', null) }}
+    {{ Form::submit('Create New Post') }}
+{!! Form::close() !!}
 
 </head>
 </body>
